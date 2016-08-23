11c8df4ed0dd1cba31c3599e8f850a98e21ca564
Marcelo Olivas <mfolivas@gmail.com>
2016-08-23 14:37:20 -0400

    Added the scaffolding for the project.

---
+++ src/main/java/com/mfolivas/caishen/CaishenApplication.java
@@ -0,0 +1,12 @@
+package com.mfolivas.caishen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaishenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaishenApplication.class, args);
+	}
+}
